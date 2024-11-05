9ad44280f7f9a31c56c61d10e435a57f1616f61c
IFINFO\sunde <bgjung@ifinfo.co.kr>
2024-11-05 09:12:48 +0900

    add front

--- src/main/java/com/example/scheduler/controller/AuthCheckController.java
+++ src/main/java/com/example/scheduler/controller/AuthCheckController.java
@@ -1,2 +1,20 @@
-package com.example.scheduler.controller;public class AuthCheckController {
+package com.example.scheduler.controller;
+
+import com.example.scheduler.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AuthCheckController {
+
+    @PostMapping("/check")
+    public ResponseEntity<User> checkAuth() {
+
+        return ResponseEntity.ok().build();
+    }
 }
